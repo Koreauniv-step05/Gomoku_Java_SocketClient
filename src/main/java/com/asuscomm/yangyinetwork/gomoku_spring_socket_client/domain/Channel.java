f96bf3cee0ce07b775c15ce0e1f706b439999e1c
pjo901018 <pjo901018@korea.ac.kr>
2017-05-07 20:43:02 +0900

    서버 모델 변화에 따른 수정

--- src/main/java/com/asuscomm/yangyinetwork/gomoku_spring_socket_client/domain/Channel.java
+++ src/main/java/com/asuscomm/yangyinetwork/gomoku_spring_socket_client/domain/Channel.java
@@ -3,33 +3,32 @@ package com.asuscomm.yangyinetwork.gomoku_spring_socket_client.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.asuscomm.yangyinetwork.gomoku_spring_socket_client.domain.Channel.Status.NEEDS_MORE_USER;
+import static com.asuscomm.yangyinetwork.gomoku_spring_socket_client.domain.Channel.Status.NEEDS_ONLY_OBSERVER;
+
 /**
  * Created by jaeyoung on 2017. 5. 7..
  */
 public class Channel {
     List<User> userList;
     String id;
-    String status;
 
     public interface Status {
         String NEEDS_MORE_USER = "NEEDS_MORE_USER";
         String NEEDS_ONLY_OBSERVER = "NEEDS_ONLY_OBSERVER";
-        String ALREADY_STARTED = "ALREADY_STARTED";
     }
 
     public Channel() {
     }
 
-    public Channel(String id, String status) {
+    public Channel(String id) {
         this.userList = new ArrayList<User>();
         this.id = id;
-        this.status = status;
     }
 
-    public Channel(List<User> userList, String id, String status) {
+    public Channel(List<User> userList, String id) {
         this.userList = userList;
         this.id = id;
-        this.status = status;
     }
 
     public List<User> getUserList() {
@@ -49,11 +48,12 @@ public class Channel {
     }
 
     public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
+        if(userList.size() >= 2) {
+            return NEEDS_ONLY_OBSERVER;
+        } else {
+            return NEEDS_MORE_USER;
+        }
     }
 }
+
 
--- src/main/java/com/asuscomm/yangyinetwork/gomoku_spring_socket_client/domain/User.java
+++ src/main/java/com/asuscomm/yangyinetwork/gomoku_spring_socket_client/domain/User.java
@@ -3,5 +3,21 @@ package com.asuscomm.yangyinetwork.gomoku_spring_socket_client.domain;
 /**
  * Created by jaeyoung on 2017. 5. 7..
  */
-public class User {
+public class User  {
+    String name;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
