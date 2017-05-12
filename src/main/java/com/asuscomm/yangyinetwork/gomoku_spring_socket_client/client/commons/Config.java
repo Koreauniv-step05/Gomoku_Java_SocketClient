a9dd4e4a94f7a22637b514ea3a4ef0ba9be80ade
pjo901018 <pjo901018@korea.ac.kr>
2017-05-12 18:18:09 +0900

    remosthost로 변경

--- src/main/java/com/asuscomm/yangyinetwork/gomoku_spring_socket_client/client/commons/Config.java
+++ src/main/java/com/asuscomm/yangyinetwork/gomoku_spring_socket_client/client/commons/Config.java
@@ -5,8 +5,8 @@ package com.asuscomm.yangyinetwork.gomoku_spring_socket_client.client.commons;
  */
 public interface Config {
     String SOCKET_URL = "ws://{host}:{port}/gomoku";
-    String SOCKET_HOST = "localhost";int SOCKET_PORT =8080;
-//    String SOCKET_HOST = "52.78.111.146"; int SOCKET_PORT = 80;
+//    String SOCKET_HOST = "localhost";int SOCKET_PORT =8080;
+    String SOCKET_HOST = "52.78.111.146"; int SOCKET_PORT = 80;
 
     String SOCKET_CHANNEL = "33";
 
